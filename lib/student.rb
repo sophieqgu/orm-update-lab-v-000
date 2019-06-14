5beac19229ccdf4d1c1cf4104728725190c53c1e
Sophie Gu <sophieqgu@gmail.com>
2019-06-14 02:58:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
   
   
   def self.new_from_db(row)
-    student = self.new 
+    student = self.new(name, grade)
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
