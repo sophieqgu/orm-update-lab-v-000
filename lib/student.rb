cc990fadf11822057f4a39346d30cd34b6ad925d
Sophie Gu <sophieqgu@gmail.com>
2019-06-14 03:02:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,10 +60,11 @@ class Student
     sql = <<-SQL 
       SELECT * FROM students 
       WHERE name = ? 
+      LIMIT 1 
     SQL
     DB[:conn].execute(sql, name).map do |row| 
       self.new_from_db(row)
-    end 
+    end.first 
   end 
   
   
